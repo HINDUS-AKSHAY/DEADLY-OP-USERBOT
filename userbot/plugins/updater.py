27cef21e16b290fd3277e15da4350ff214032c45
sameerpanthi <75129693+sameerpanthi@users.noreply.github.com>
2021-05-28 19:26:33 +0530

    Update updater.py

--- userbot/plugins/updater.py
+++ userbot/plugins/updater.py
@@ -15,13 +15,13 @@ IS_SELECTED_DIFFERENT_BRANCH = (
     "please check out to an official branch, and re-start the updater."
 )
 OFFICIAL_UPSTREAM_REPO = Config.UPSTREAM_REPO
-BOT_IS_UP_TO_DATE = "**The 𝕄𝔸𝔽𝕀𝔸𝔹𝕆𝕋** is up-to-date sur."
+BOT_IS_UP_TO_DATE = "**The ELITES BOT** is up-to-date sur."
 NEW_BOT_UP_DATE_FOUND = (
     "new update found for {branch_name}\n"
     "changelog: \n\n{changelog}\n"
-    "updating your 𝕄𝔸𝔽𝕀𝔸𝔹𝕆𝕋 ..."
+    "updating your ELITES BOT ..."
 )
-NEW_UP_DATE_FOUND = "New update found for {branch_name}\n" "`updating your 𝕄𝔸𝔽𝕀𝔸𝔹𝕆𝕋...`"
+NEW_UP_DATE_FOUND = "New update found for {branch_name}\n" "`updating your ELITES BOT...`"
 REPO_REMOTE_NAME = "temponame"
 IFFUCI_ACTIVE_BRANCH_NAME = "master"
 DIFF_MARKER = "HEAD..{remote_name}/{branch_name}"
@@ -137,10 +137,9 @@ def generate_change_log(git_repo, diff_marker):
 async def deploy_start(tgbot, message, refspec, remote):
     await message.edit(RESTARTING_APP)
     await message.edit(
-        "Updated your 𝕄𝔸𝔽𝕀𝔸𝔹𝕆𝕋 successfully sur!!!\nNow type `.ping` after 5 mins to check if I'm on🚶😏"
+        "Uᴘᴅᴀᴛᴇᴅ Yᴏᴜʀ ELITES 2.0 Bᴏᴛ Is Sᴜᴄᴄᴇssғᴜʟʟʏ. Aғᴛᴇʀ 5-7 Mɪɴᴜᴛᴇs Tʏᴘᴇ (.ping or .alive) Tᴏ Cʜᴇᴄᴋ I ᴀᴍ Aʟɪᴠᴇ Oʀ ɴᴏᴛ🚶😏"
     )
     await remote.push(refspec=refspec)
     await tgbot.disconnect()
     os.execl(sys.executable, sys.executable, *sys.argv)
-  
   
