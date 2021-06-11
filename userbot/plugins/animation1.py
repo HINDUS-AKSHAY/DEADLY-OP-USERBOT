8f785dc18ff0b3bbfb1577e44437cff7fbca805e
sameerpanthi <75129693+sameerpanthi@users.noreply.github.com>
2021-06-11 15:55:55 +0530

    Update animation1.py

--- userbot/plugins/animation1.py
+++ userbot/plugins/animation1.py
@@ -7,7 +7,7 @@ from userbot import *
 from deadlybot.utils import admin_cmd, edit_or_reply, sudo_cmd
 from userbot.cmdhelp import CmdHelp
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Mafia User"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Deadly User"
 
 @bot.on(admin_cmd(pattern=f"indflag$", outgoing=True))
 @bot.on(sudo_cmd(pattern=f"indflag$", allow_sudo=True))
