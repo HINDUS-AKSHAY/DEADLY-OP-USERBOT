20b02d9e63c28af20f2f799588647224a93fba87
sameerpanthi <75129693+sameerpanthi@users.noreply.github.com>
2021-06-11 15:03:41 +0530

    Update memes.py

--- userbot/plugins/memes.py
+++ userbot/plugins/memes.py
@@ -8,7 +8,7 @@ from telethon.tl.functions.users import GetFullUserRequest
 from telethon.tl.types import MessageEntityMentionName
 
 from userbot.cmdhelp import CmdHelp
-from savagebot.utils import admin_cmd, sudo_cmd, edit_or_reply
+from deadlybot.utils import admin_cmd, sudo_cmd, edit_or_reply
 
 # ================= CONSTANT =================
 
