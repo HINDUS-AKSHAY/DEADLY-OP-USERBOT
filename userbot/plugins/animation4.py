78112cf682b20835405ddd48aad71b48797251a4
sameerpanthi <75129693+sameerpanthi@users.noreply.github.com>
2021-06-11 16:05:22 +0530

    Update animation4.py

--- userbot/plugins/animation4.py
+++ userbot/plugins/animation4.py
@@ -1,11 +1,29 @@
 import asyncio
-
+from collections import deque
+from telethon.tl.functions.users import GetFullUserRequest
+from userbot import *
+from userbot.cmdhelp import CmdHelp
 from userbot import ALIVE_NAME, CMD_HELP
 from deadlybot.utils import admin_cmd, edit_or_reply, sudo_cmd
-from userbot.cmdhelp import CmdHelp
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "deadly user"
 
+@bot.on(admin_cmd(pattern="gn$", outgoing=True))
+@bot.on(sudo_cmd(pattern="gn$", allow_sudo=True))
+async def gn(event):
+    if event.fwd_from:
+        return
+    await edit_or_reply(event, "｡♥｡･ﾟ♡ﾟ･｡♥｡･｡･｡･｡♥｡･\n╱╱╱╱╱╱╱╭╮╱╱╱╭╮╱╭╮╭╮\n╭━┳━┳━┳╯┃╭━┳╋╋━┫╰┫╰╮\n┃╋┃╋┃╋┃╋┃┃┃┃┃┃╋┃┃┃╭┫\n┣╮┣━┻━┻━╯╰┻━┻╋╮┣┻┻━╯\n╰━╯╱╱╱╱╱╱╱╱╱╱╰━╯\n｡♥｡･ﾟ♡ﾟ･｡♥° ♥｡･ﾟ♡ﾟ･"
+    )
+
+
+@bot.on(admin_cmd(pattern="gm$", outgoing=True))
+@bot.on(sudo_cmd(pattern="gm$", allow_sudo=True))
+async def gm(event):
+    if event.fwd_from:
+        return
+    await edit_or_reply(event, "｡♥｡･ﾟ♡ﾟ･｡♥｡･｡･｡･｡♥｡･｡♥｡･ﾟ♡ﾟ･\n╱╱╱╱╱╱╱╭╮╱╱╱╱╱╱╱╱╱╱╭╮\n╭━┳━┳━┳╯┃╭━━┳━┳┳┳━┳╋╋━┳┳━╮\n┃╋┃╋┃╋┃╋┃┃┃┃┃╋┃╭┫┃┃┃┃┃┃┃╋┃\n┣╮┣━┻━┻━╯╰┻┻┻━┻╯╰┻━┻┻┻━╋╮┃\n╰━╯╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╰━╯\n｡♥｡･ﾟ♡ﾟ･｡♥｡･｡･｡･｡♥｡･｡♥｡･ﾟ♡ﾟ･"
+    )
 
 @bot.on(admin_cmd(outgoing=True, pattern="kiler( (.*)|$)"))
 @bot.on(sudo_cmd(pattern="kiler( (.*)|$)", allow_sudo=True))
