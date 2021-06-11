7d25f1edb6fdfff513b04c30524a475be6104a58
sameerpanthi <75129693+sameerpanthi@users.noreply.github.com>
2021-06-11 17:49:02 +0530

    Update _core.py

--- userbot/_core.py
+++ userbot/_core.py
@@ -71,7 +71,7 @@ async def install(event):
                             a = "__Installing...__"
                             b = 1
                         await event.edit(a)
-                    return await event.edit(f"✅ **Installed module** :- `{shortname}` \n✨ BY :- {deadly}\n\n{string}\n\n        ⚡ **[LEGENDARY AF DeadlyBot](t.me/elites_userbot)** ⚡", link_preview=False)
+                    return await event.edit(f"✅ **Installed module** :- `{shortname}` \n✨ BY :- {deadly}\n\n{string}\n\n        ⚡ **[LEGENDARY AF DeadlyBot](t.me/deadly_userbot)** ⚡", link_preview=False)
                 return await event.edit(f"Installed module `{os.path.basename(downloaded_file_name)}`")
             else:
                 os.remove(downloaded_file_name)
