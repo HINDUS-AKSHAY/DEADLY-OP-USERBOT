b31179fde7cc01b40ce19abd2efecff5d4d5f1fd
sameerpanthi <75129693+sameerpanthi@users.noreply.github.com>
2021-06-11 13:48:21 +0530

    Update ping.py

--- userbot/plugins/ping.py
+++ userbot/plugins/ping.py
@@ -5,7 +5,7 @@ from .. import ALIVE_NAME, CMD_HELP
 from ..utils import admin_cmd, edit_or_reply, sudo_cmd
 from ..cmdhelp import CmdHelp
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Mafia User"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Deadly User"
 h1m4n5hu0p = borg.uid
 
 
