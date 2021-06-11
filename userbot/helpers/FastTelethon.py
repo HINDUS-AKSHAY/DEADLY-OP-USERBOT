f68b78fd4b7984c811562bbea9390de3e62857b4
sameerpanthi <75129693+sameerpanthi@users.noreply.github.com>
2021-06-11 13:52:32 +0530

    Update FastTelethon.py

--- userbot/helpers/FastTelethon.py
+++ userbot/helpers/FastTelethon.py
@@ -1,6 +1,4 @@
-# credits
-# thanks to catbot
-# porting in MafiaBot
+
 """
 > Based on parallel_file_transfer.py from mautrix-telegram, with permission to distribute under the MIT license
 > Copyright (C) 2019 Tulir Asokan - https://github.com/tulir/mautrix-telegram
