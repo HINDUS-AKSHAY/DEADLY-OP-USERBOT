936fe3220156e1bae393580e4acdea94c8660eeb
sameerpanthi <75129693+sameerpanthi@users.noreply.github.com>
2021-06-13 17:48:25 +0530

    Update _helper.py

--- userbot/plugins/_helper.py
+++ userbot/plugins/_helper.py
@@ -1,3 +1,4 @@
+
 from userbot.Config import Config
 import asyncio
 
@@ -9,13 +10,13 @@ from deadlybot.utils import admin_cmd, edit_or_reply, sudo_cmd
 
 
 @bot.on(admin_cmd(pattern="help ?(.*)", outgoing=True))
-async def info(event):
+async def yardim(event):
     if event.fwd_from:
         return
     tgbotusername = Config.TG_BOT_USER_NAME_BF_HER
     input_str = event.pattern_match.group(1)
-    if tgbotusername is not None or deadly_input == "text":
-        results = await event.client.inline_query(tgbotusername, "@deadly_sameer_bot")
+    if tgbotusername is not None or savagebot_input == "text":
+        results = await event.client.inline_query(tgbotusername, "@savage_userbot")
         await results[0].click(
             event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
         )
@@ -40,9 +41,9 @@ async def info(event):
     input_str = event.pattern_match.group(1)
     if input_str == "text":
         string = (
-            "Total {count} commands found in {plugincount} sudo plugins of DeadlyBot\n\n"
+            "Total {count} commands found in {plugincount} sudo plugins of savage bot\n\n"
         )
-        deadlycount = 0
+        savagebotcount = 0
         plugincount = 0
         for i in sorted(SUDO_LIST):
             plugincount += 1
@@ -50,10 +51,10 @@ async def info(event):
             for iter_list in SUDO_LIST[i]:
                 string += "    " + str(iter_list)
                 string += "\n"
-                deadlycount += 1
+                savagebotcount += 1
             string += "\n"
         if len(string) > 4095:
-            data = string.format(count=deadlycount, plugincount=plugincount)
+            data = string.format(count=savagebotcount, plugincount=plugincount)
             key = (
                 requests.post(
                     "https://nekobin.com/api/documents", json={"content": data}
@@ -63,23 +64,23 @@ async def info(event):
                 .get("key")
             )
             url = f"https://nekobin.com/{key}"
-            reply_text = f"All commands of the DeadlyBot are [here]({url})"
+            reply_text = f"All commands of the savage bot are [here]({url})"
             await event.reply(reply_text, link_preview=False)
             return
         await event.reply(
-            string.format(count=deadlycount, plugincount=plugincount), link_preview=False
+            string.format(count=savagebotcount, plugincount=plugincount), link_preview=False
         )
         return
     if input_str:
         if input_str in SUDO_LIST:
             string = "<b>{count} Commands found in plugin {input_str}:</b>\n\n"
-            deadlycount = 0
+            savagebotcount = 0
             for i in SUDO_LIST[input_str]:
                 string += f"  •  <code>{i}</code>"
                 string += "\n"
-                deadlycount += 1
+                savagebotcount += 1
             await event.reply(
-                string.format(count=deadlycount, input_str=input_str), parse_mode="HTML"
+                string.format(count=savagebotcount, input_str=input_str), parse_mode="HTML"
             )
         else:
             reply = await event.reply(input_str + " is not a valid plugin!")
@@ -90,9 +91,9 @@ async def info(event):
         string = "<b>Please specify which plugin do you want help for !!\
             \nNumber of plugins : </b><code>{count}</code>\
             \n<b>Usage:</b> <code>.help plugin name</code>\n\n"
-        deadlycount = 0
+        savagebotcount = 0
         for i in sorted(SUDO_LIST):
             string += "≈ " + f"<code>{str(i)}</code>"
             string += " "
-            deadlycount += 1
-        await event.reply(string.format(count=deadlycount), parse_mode="HTML")
+            savagebotcount += 1
+        await event.reply(string.format(count=savagebotcount), parse_mode="HTML")
