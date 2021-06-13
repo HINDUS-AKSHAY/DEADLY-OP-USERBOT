5ab95f315d66e27153aa9762d28aab54c569dcc8
sameerpanthi <75129693+sameerpanthi@users.noreply.github.com>
2021-06-13 19:43:39 +0530

    Update pmpermit.py

--- userbot/plugins/pmpermit.py
+++ userbot/plugins/pmpermit.py
@@ -31,11 +31,11 @@ opsameer = (
     else "**YOU HAVE TRESPASSED TO MY MASTERS INBOX** \n THIS IS ILLEGAL AND REGARDED AS CRIME"
 )
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Deadly user"
-USER_BOT_WARN_ZERO = "**hey nub nibba don't spam here .. oh u want to spam here ? Then I am going to block u nibba**\n__Now GTFO, my master is busy__"
+USER_BOT_WARN_ZERO = "hey nub nibba don't spam here .. oh u want to spam here ? Then I am going to block u nibba**\n__Now GTFO, my master is busy__"
 USER_BOT_NO_WARN = (
-    "𝐇𝐄𝐋𝐋𝐎, 𝐓𝐇𝐈𝐒 𝐈𝐒 𝐃𝐄𝐀𝐃𝐋𝐘 𝐁𝐎𝐓 𝐔𝐋𝐓𝐑𝐀 𝐏𝐑𝐈𝐕𝐀𝐓𝐄 𝐒𝐄𝐂𝐔𝐑𝐈𝐓𝐘 𝐏𝐑𝐎𝐓𝐎𝐂𝐎𝐎𝐋 ⚠️**\n"
-    f"\n**{opsameer}**\n\n"
-    "To start a valid conversation\n\n⚔️Register Your Request!⚔️\n\nSend `/start` To Register Your Request\nHopefully u will get a reply⚜️"
+    "⚠️ 𝐇𝐄𝐋𝐋𝐎, 𝐓𝐇𝐈𝐒 𝐈𝐒 𝐃𝐄𝐀𝐃𝐋𝐘 𝐁𝐎𝐓 𝐔𝐋𝐓𝐑𝐀 𝐏𝐑𝐈𝐕𝐀𝐓𝐄 𝐒𝐄𝐂𝐔𝐑𝐈𝐓𝐘 𝐏𝐑𝐎𝐓𝐎𝐂𝐎𝐎𝐋 ⚠️**\n"
+    f"\n{opsameer}\n\n"
+    "To start a valid conversation\n🔸Register Your Request!🔹\n🔸Send `/start` To Register🔹\n🔸Hopefully u will get a reply🔹"
 )
               
 if Var.PRIVATE_GROUP_ID is not None:
