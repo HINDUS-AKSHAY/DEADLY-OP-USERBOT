dc581a91b810cee3a416a482b5eaddcfaaba89c9
sameerpanthi <75129693+sameerpanthi@users.noreply.github.com>
2021-06-13 22:38:11 +0530

    Update _helper.py

--- userbot/plugins/_helper.py
+++ userbot/plugins/_helper.py
@@ -8,6 +8,31 @@ from userbot import ALIVE_NAME, CMD_LIST, SUDO_LIST
 from deadlybot.utils import admin_cmd, edit_or_reply, sudo_cmd
 
 
+@bot.on(admin_cmd(pattern="help ?(.*)", outgoing=True))
+async def yardim(event):
+    if event.fwd_from:
+        return
+    tgbotusername = Config.TG_BOT_USER_NAME_BF_HER
+    input_str = event.pattern_match.group(1)
+    if tgbotusername is not None or mafia_input == "text":
+        results = await event.client.inline_query(tgbotusername, "@MafiaBot_Support")
+        await results[0].click(
+            event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
+        )
+        await event.delete()
+    else:
+        await edit_or_reply(event, ["NO_BOT"])
+    
+        if input_str in CMD_LIST:
+          string = "Commands found in {}:\n".format(input_str)
+          for i in CMD_LIST[input_str]:
+              string += "  " + i
+              string += "\n"
+          await event.edit(string)
+        else:
+          await event.edit(input_str + " is not a valid plugin!")
+
+
 
 @bot.on(sudo_cmd(allow_sudo=True, pattern="help ?(.*)"))
 async def info(event):
