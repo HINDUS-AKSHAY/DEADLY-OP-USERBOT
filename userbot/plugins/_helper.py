6f1d57445e112cfe7fe56ea7b92ac06ecef2479a
Amaan The Gamer King <73664034+Javes786@users.noreply.github.com>
2021-06-16 23:29:56 +0530

    Update _helper.py

--- userbot/plugins/_helper.py
+++ userbot/plugins/_helper.py
@@ -7,125 +7,102 @@ from deadlybot.utils import admin_cmd
 from userbot import ALIVE_NAME, CMD_HELP, CMD_LIST
 from userbot import bot as borg
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Noob"
+# Dc ©
+from userbot import CMD_LIST
+from userbot import ALIVE_NAME
+from userbot.utils import admin_cmd, sudo_cmd
+from platform import uname
+import sys
+from telethon import events, functions, __version__
 
-HELPTYPE=False
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Noob"
 
-
-
-@borg.on(admin_cmd(outgoing=True, pattern="help ?(.*)"))
+#@command(pattern="^.help ?(.*)")
+@borg.on(admin_cmd(pattern=r"help ?(.*)", outgoing=True))
+@borg.on(sudo_cmd(pattern=r"help ?(.*)", outgoing=True, allow_sudo=True))
 async def cmd_list(event):
-    global HELPTYPE
-    reply_to_id = None
-    if event.reply_to_msg_id:
-        reply_to_id = event.reply_to_msg_id
-    input_str = event.pattern_match.group(1)
-    if input_str == "text":
-        string = (
-            "Total {count} commands found in {plugincount} plugins of userbot\n\n"
-        )
-        catcount = 0
-        plugincount = 0
-        for i in sorted(CMD_LIST):
-            plugincount += 1
-            string += f"{plugincount}) Commands found in Plugin " + i + " are \n"
-            for iter_list in CMD_LIST[i]:
-                string += "    " + str(iter_list)
+    if not event.text[0].isalpha() and event.text[0] not in ("/" , "#", "-", "_", "@"):
+        tgbotusername = Var.TG_BOT_USER_NAME_BF_HER
+        input_str = event.pattern_match.group(1)
+        if tgbotusername is None or input_str == "text":
+            string = ""
+            for i in CMD_LIST:
+                string += "⚡️" + i + "\n"
+                for iter_list in CMD_LIST[i]:
+                    string += "    `" + str(iter_list) + "`"
+                    string += "\n"
                 string += "\n"
-                catcount += 1
-            string += "\n"
-        if len(string) > 4095:
-            data = string.format(count=catcount, plugincount=plugincount)
-            key = (
-                requests.post(
-                    "https://nekobin.com/api/documents", json={"content": data}
-                )
-                .json()
-                .get("result")
-                .get("key")
-            )
-            url = f"https://nekobin.com/{key}"
-            reply_text = f"**All commands of the userbot can be seen [here]({url})**"
-            await event.edit(reply_text)
-            return
-        await event.edit(string.format(count=catcount, plugincount=plugincount))
-        return
-    if input_str:
-        if input_str in CMD_LIST:
-            string = "<b>{count} Commands found in plugin {input_str}:</b>\n\n"
-            catcount = 0
-            for i in CMD_LIST[input_str]:
-                string += f"  •  <code>{i}</code>"
-                string += "\n"
-                catcount += 1
-            await event.edit(
-                string.format(count=catcount, input_str=input_str), parse_mode="HTML"
-            )
+            if len(string) > 69:
+                with io.BytesIO(str.encode(string)) as out_file:
+                    out_file.name = "cmd.txt"
+                    await bot.send_file(
+                        event.chat_id,
+                        out_file,
+                        force_document=True,
+                        allow_cache=False,
+                        caption="**COMMANDS** In DEADLY USERBOT",
+                        reply_to=reply_to_id
+                    )
+                    await event.delete()
+            else:
+                await event.edit(string)
+        elif input_str:
+            if input_str in CMD_LIST:
+                string = "Commands found in {}:\n".format(input_str)
+                for i in CMD_LIST[input_str]:
+                    string += "  " + i
+                    string += "\n"
+                await event.edit(string)
+            else:
+                await event.edit(input_str + " is not a valid plugin!")
         else:
-            await event.edit(input_str + " is not a valid plugin!")
-            await asyncio.sleep(3)
-            await event.delete()
-    else:
-        if HELPTYPE is True:
-            help_string = f"Userbot Helper.. Provided by {DEFAULTUSER}\
-                          \nUserbot Helper to reveal all the plugin names"
-                          
-            tgbotusername = Var.TG_BOT_USER_NAME_BF_HER
+            help_string = f"""Userbot Helper.. Provided by ✨{DEFAULTUSER}✨ \n
+`Userbot Helper to reveal all the commands`\n__Do .help plugin_name for commands, in case popup doesn't appear.__"""
             results = await bot.inline_query(  # pylint:disable=E0602
-                tgbotusername, help_string
+                tgbotusername,
+                help_string
             )
-            await results[0].click(event.chat_id, reply_to=reply_to_id, hide_via=True)
+            await results[0].click(
+                event.chat_id,
+                reply_to=event.reply_to_msg_id,
+                hide_via=True
+            )
             await event.delete()
-        else:
-            string = "<b>Please specify which plugin do you want help for !!\
-                \nNumber of plugins : </b><code>{count}</code>\
-                \n<b>Usage:</b> <code>.help</code> plugin name\n\n"
-            catcount = 0
-            for i in sorted(CMD_LIST):
-                string += "• " + f"<code>{str(i)}</code>"
-                string += "   "
-                catcount += 1
-            await event.edit(string.format(count=catcount), parse_mode="HTML")
-
-
-
-
-@borg.on(admin_cmd(outgoing=True, pattern="chk ?(.*)"))
-
-async def info(event):
-    """ For .info command,"""
-    args = event.pattern_match.group(1).lower()
-    if args:
-        if args in CMD_HELP:
-            await event.edit(str(CMD_HELP[args]))
-        else:
-            event = await event.edit("Please specify a valid plugin name.")
-            await asyncio.sleep(4)
-            await event.delete()
-    else:
-        string = "<b>⚠️ kam plugins he but jitne he ab kaam ke he ⚠️ !!\
-            \nNumber of plugins : </b><code>{count}</code>\
-            \n<b>Usage : </b><code>.chk</code> <plugin name>\n\n"
-        deadlycount = 0
-        for i in sorted(CMD_HELP):
-            string += "• " + f"<code>{str(i)}</code>"
-            string += "   "
-            deadlycount += 1
-       
-            await event.edit(string.format(count=deadlycount), parse_mode="HTML")
-
-
-'''@borg.on(admin_cmd(pattern="dc$"))
+            
+@borg.on(admin_cmd(pattern="dc"))  # pylint:disable=E0602
 async def _(event):
-    result = await bot(functions.help.GetNearestDcRequest())
-    result = (
-        yaml_format(result)
-        + "\n\n**List Of Telegram Data Centres:**\
-                \nDC1 : Miami FL, USA\
-                \nDC2 : Amsterdam, NL\
-                \nDC3 : Miami FL, USA\
-                \nDC4 : Amsterdam, NL\
-                \nDC5 : Singapore, SG\
-                "
-    )
-    await event.edit(result)
-'''
+    if event.fwd_from:
+        return
+    result = await borg(functions.help.GetNearestDcRequest())  # pylint:disable=E0602
+    await event.edit(result.stringify())
+
+
+@borg.on(admin_cmd(pattern="config"))  # pylint:disable=E0602
+async def _(event):
+    if event.fwd_from:
+        return
+    result = await borg(functions.help.GetConfigRequest())  # pylint:disable=E0602
+    result = result.stringify()
+    logger.info(result)  # pylint:disable=E0602
+    await event.edit("Telethon UserBot powered byDark_cobra")
+
+
+@borg.on(admin_cmd(pattern="syntax (.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    plugin_name = event.pattern_match.group(1)
+
+    if plugin_name in CMD_LIST:
+        help_string = CMD_LIST[plugin_name].__doc__
+        unload_string = f"Use `.unload {plugin_name}` to remove this plugin.\n           © Dark Cobra"
+        
+        if help_string:
+            plugin_syntax = f"Syntax for plugin **{plugin_name}**:\n\n{help_string}\n{unload_string}"
+        else:
+            plugin_syntax = f"No DOCSTRING has been setup for {plugin_name} plugin."
+    else:
+
+        plugin_syntax = "Enter valid **Plugin** name.\nDo `.plinfo` or `.help` to get list of valid plugin names."
+
+    await event.edit(plugin_syntax)
