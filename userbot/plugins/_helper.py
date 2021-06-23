851c4ec065f030d8be930ebd8f21becb3f8623ff
sameerpanthi <75129693+sameerpanthi@users.noreply.github.com>
2021-06-23 18:34:56 +0530

    Update _helper.py

--- userbot/plugins/_helper.py
+++ userbot/plugins/_helper.py
@@ -13,8 +13,8 @@ async def yardim(event):
         return
     tgbotusername = Config.TG_BOT_USER_NAME_BF_HER
     input_str = event.pattern_match.group(1)
-    if tgbotusername is not None or mafia_input == "text":
-        results = await event.client.inline_query(tgbotusername, "@MafiaBot_Support")
+    if tgbotusername is not None or deadly_input == "text":
+        results = await event.client.inline_query(tgbotusername, "@deadly_techy")
         await results[0].click(
             event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
         )
