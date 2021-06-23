e2ace4fa809da0f34324ff120ab7be618acc60a1
sameerpanthi <75129693+sameerpanthi@users.noreply.github.com>
2021-06-23 20:57:16 +0530

    Create adultzone.py

---
+++ userbot/plugins/adultzone.py
@@ -0,0 +1,52 @@
+import asyncio
+import os
+import urllib
+
+import requests
+
+from userbot import *
+from deadlybot.utils import *
+from userbot.cmdhelp import CmdHelp
+
+@bot.on(admin_cmd("boobs$"))
+@bot.on(sudo_cmd(pattern="boobs$", allow_sudo=True))
+async def boobs(event):
+    if event.fwd_from:
+        return
+    if not os.path.isdir(Var.TEMP_DOWNLOAD_DIRECTORY):
+        os.makedirs(Var.TEMP_DOWNLOAD_DIRECTORY)
+    pic_loc = os.path.join(Var.TEMP_DOWNLOAD_DIRECTORY, "bobs.jpg")
+    a = await event.reply("Hn Ruk Tharki Tere Liye Boobs Dhoodh Rha Hu🧐")
+    await asyncio.sleep(0.5)
+    await a.edit("HN RUKJA THARKI DE RHA HU🤪")
+    nsfw = requests.get("http://api.oboobs.ru/noise/1").json()[0]["preview"]
+    urllib.request.urlretrieve("http://media.oboobs.ru/{}".format(nsfw), pic_loc)
+    await event.client.send_file(event.chat_id, pic_loc, force_document=False)
+    os.remove(pic_loc)
+    await event.delete()
+    await a.delete()
+
+
+@bot.on(admin_cmd("butts$"))
+@bot.on(sudo_cmd(pattern="butts$", allow_sudo=True))
+async def butts(event):
+    if event.fwd_from:
+        return
+    if not os.path.isdir(Var.TEMP_DOWNLOAD_DIRECTORY):
+        os.makedirs(Var.TEMP_DOWNLOAD_DIRECTORY)
+    pic_loc = os.path.join(Var.TEMP_DOWNLOAD_DIRECTORY, "butts.jpg")
+    a = await event.reply("HN THARKI DETA HU TUJHE BUTTS KU PIC🧐")
+    await asyncio.sleep(0.5)
+    await a.edit("HN RUKJA THARKI BUTTS KI PIC DERA HU🤪")
+    nsfw = requests.get("http://api.obutts.ru/noise/1").json()[0]["preview"]
+    urllib.request.urlretrieve("http://media.obutts.ru/{}".format(nsfw), pic_loc)
+    await event.client.send_file(event.chat_id, pic_loc, force_document=False)
+    os.remove(pic_loc)
+    await event.delete()
+    await a.delete()
+
+CmdHelp("adultzone").add_command(
+  'boobs', None, 'Sends a random boobs pic'
+).add_command(
+  'butts', None, 'Sends a random Butt pic'
+).add()
