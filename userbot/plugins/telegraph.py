eba21e178f306a1d04115c4ff2cff2513fbe3478
sameerpanthi <75129693+sameerpanthi@users.noreply.github.com>
2021-06-28 15:56:34 +0530

    Update telegraph.py

--- userbot/plugins/telegraph.py
+++ userbot/plugins/telegraph.py
@@ -9,7 +9,6 @@ from . import *
 
 
 DEADLY_NAME = str(ALIVE_NAME) if ALIVE_NAME else "DEADLY User"
-lg_id = Config.PM_PERMIT_GROUP_ID
 
 telegraph = Telegraph()
 r = telegraph.create_account(short_name=Config.TELEGRAPH_SHORT_NAME)
