3c257578b62af0497e5cb173877bdafbedb39889
sameerpanthi <75129693+sameerpanthi@users.noreply.github.com>
2021-07-12 09:43:53 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -60,7 +60,7 @@ async def startupmessage():
             await bot.send_message(
                 Config.PRIVATE_GROUP_BOT_API_ID,
                 ("""𝐂𝐎𝐍𝐆𝐑𝐀𝐓𝐔𝐋𝐀𝐓𝐈𝐎𝐍 𝐘𝐎𝐔𝐑 𝐃𝐄𝐀𝐃𝐋𝐘𝐁𝐎𝐓 𝐁𝐎𝐓 𝐈𝐒 𝐃𝐄𝐏𝐋𝐎𝐘𝐄𝐃 𝐒𝐔𝐂𝐂𝐄𝐒𝐒𝐅𝐔𝐋𝐋𝐘 😈💖💥 .. 𝐓𝐘𝐏𝐄 (.alive or .ping) 𝐅𝐎𝐑 𝐂𝐇𝐄𝐂𝐊 𝐓𝐇𝐀𝐓 𝐁𝐎𝐓 𝐈𝐒 𝐀𝐋𝐈𝐕𝐄 𝐎𝐑 𝐍𝐎𝐓...𝐉𝐎𝐈𝐍 @deadly_userbot 𝐅𝐎𝐑 𝐀𝐍𝐘 𝐇𝐄𝐋𝐏 ..𝐄𝐍𝐉𝐎𝐘 𝐔𝐑 𝐁𝐎𝐓🤘😉.""")
-
+)
     except Exception as e:
         LOGS.info(str(e))
 
