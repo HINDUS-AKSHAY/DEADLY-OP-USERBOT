7213167ff1c860a76fb4dbf98bed6a6bf935d232
sameerpanthi <75129693+sameerpanthi@users.noreply.github.com>
2021-07-14 12:17:15 +0530

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -52,7 +52,7 @@ def button(page, modules):
     modules = CMD_HELP
 if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
     @tgbot.on(InlineQuery)  # pylint:disable=E0602
-async def inline_handler(event):
+    async def inline_handler(event):
         builder = event.builder
         result = None
         query = event.text
