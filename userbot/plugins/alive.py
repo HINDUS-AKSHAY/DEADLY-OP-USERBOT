8337ec049a2741aab22821aa2e3b86311646eebd
DEADLY-FIGHTERS <87220607+DEADLY-FIGHTERS@users.noreply.github.com>
2021-12-17 22:02:56 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -31,7 +31,7 @@ edit_time = 5
 file1 = Config.ALIVE_PIC
 """ =======================CONSTANTS====================== """
 
-pm_caption = "__                       **😎🔥 #ᗪᗴᗩᗪᒪY_Oᑎ_ᖴIᖇᗴ 😎🔥**  __\n\n"
+pm_caption = "__                       **🔥 #ᗪᗴᗩᗪᒪY_Oᑎ_ᖴIᖇᗴ 🔥**  __\n\n"
 
 pm_caption += f"               __↼🄼🄰🅂🅃🄴🅁⇀__\n**      『{DEFAULTUSER}』**\n\n"
 pm_caption += "✘ ᗩᗷOᑌT ᗰY ՏYՏTᗴᗰ ✘\n\n"
@@ -39,7 +39,7 @@ pm_caption += "➠ TᗴᒪᗴTᕼOᑎ   ➣ 1.22.0\n"
 pm_caption += "➠ Tᗴᗩᗰ ᘜᖇOᑌᑭ ➣ [𝐃𝐄𝐀𝐃𝐋𝐘](https://t.me/deadly_fighters)\n"
 pm_caption += "➠ ՏᑌᑭᑭOᖇT ᑕᕼᑎᑎᒪ ➣ [𝐉𝐎𝐈𝐍](https://t.me/DEADLY_TECHY)\n"
 pm_caption += "➠ ՏᑌᑭᑭOᖇT ᘜᖇᑭ ➣ [𝐉𝐎𝐈𝐍](https://t.me/deadly_userbot)\n"
-pm_caption += "➠ ᑕᖇᗴᗩTOᖇ ➣ [⚡𝐒𝐀𝐌𝐄𝐄𝐑⚡](t.me/sameer_795)\n\n" 
+pm_caption += "➠ ᑕᖇᗴᗩTOᖇ ➣ [𝐒𝐀𝐌𝐄𝐄𝐑](t.me/sameer_795)\n\n" 
 pm_caption += "[🔥ᗪᗴᑭᒪOY ᗪᗴᗩᗪᒪY ᗷOT🔥](https://github.com/SAMEERPANTHI/DEADLY-OP-BOT)"
                                                      
 # @command(outgoing=True, pattern="^.alive$")
