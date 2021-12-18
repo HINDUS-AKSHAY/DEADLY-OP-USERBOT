09f66cd3968afd034f1cfedbcb41a45467a16f92
DEADLY-FIGHTERS <87220607+DEADLY-FIGHTERS@users.noreply.github.com>
2021-12-18 19:19:08 +0530

    Update memes.py

--- userbot/plugins/memes.py
+++ userbot/plugins/memes.py
@@ -614,7 +614,7 @@ async def vapor(vpr):
 @bot.on(sudo_cmd(pattern=f"repo", allow_sudo=True))
 async def source(e):
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
-        await edit_or_reply(e, "[𝙲𝙻𝙸𝙲𝙺 𝙷𝙴𝚁𝙴](https://github.com/sameerPanthi/Deadly-op-bot) 𝚃𝙾 𝙾𝙿𝙴𝙽 𝚃𝙷𝙸𝚂 \n🔥**𝙻𝙸𝚃 𝙰𝙵!!**🔥 𝙳𝙴𝙰𝙳𝙻𝚈𝙱𝙾𝚃.\n\n[👑 𝙳𝙴𝙰𝙳𝙻𝚈 𝙵𝙸𝙶𝙷𝚃𝙴𝚁𝚂 👑](t.ME/DEADLY_FIGHTERS)")
+        await edit_or_reply(e, "[𝙲𝙻𝙸𝙲𝙺 𝙷𝙴𝚁𝙴](https://github.com/DEADLY-FIGHTERS/Deadly-op-bot) 𝚃𝙾 𝙾𝙿𝙴𝙽 𝚃𝙷𝙸𝚂 \n🔥**𝙻𝙸𝚃 𝙰𝙵!!**🔥 𝙳𝙴𝙰𝙳𝙻𝚈𝙱𝙾𝚃.\n\n[👑 𝙳𝙴𝙰𝙳𝙻𝚈 𝙵𝙸𝙶𝙷𝚃𝙴𝚁𝚂 👑](t.ME/DEADLY_FIGHTERS)")
 
 
 @bot.on(admin_cmd(pattern="str(?: |$)(.*)", outgoing=True))
